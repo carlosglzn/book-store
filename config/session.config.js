5d77bcd826f2d56f85830199c488eaa537096386
Carlos Gonzalez <carlosgonzalezn19@gmail.com>
2021-07-28 12:10:52 -0500

    change in cookie duration

--- config/session.config.js
+++ config/session.config.js
@@ -14,7 +14,7 @@ module.exports = app => {
                 sameSite: process.env.NODE_ENV === 'production' ? "none" : "lax",
                 secure: process.env.NODE_ENV === 'production',
                 httpOnly: true,
-                maxAge: 60000
+                maxAge: 24 * 60 * 60 * 1000 // 24 hours
             },
             store: MongoStore.create({
                 mongoUrl: process.env.MONGODB_URI
